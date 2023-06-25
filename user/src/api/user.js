f1983b13487c77e96f2b2d8251b61d9943c36767
ROSHAN BHUVAD <roshanbhuvad15@gmail.com>
2023-06-25 19:31:40 +0530

    Designed and added user API routes and service logs

---
+++ user/src/api/user.js
@@ -0,0 +1,53 @@
+const UserService = require("../services/user-service");
+const { SubscribeToMessage } = require("../utils");
+
+const UserAuth = require("./middlewares/auth")
+
+module.exports = (app, channel) => {
+    const service = new UserService();
+    SubscribeToMessage(channel, service);
+
+    app.post("/sighup", async (req, res,next) => {
+        try {
+            const {email, password, phone} = req.body;
+
+            const {data} = await service.SignUp({email, password, phone});
+            return res.json(data)
+        } catch (err) {
+            next(err)
+        }
+    });
+
+    app.post("/login", async (req, res, next) => {
+        try {
+            const {email, password} = req.body;
+            const {data} = await service.SignIn({email, password});
+            return res.json(data)
+        }
+        catch (err) {
+            next(err)
+        }
+    });
+
+    app.get("/shoping-details", UserAuth, async (req, res, next) => {
+        try {
+          const { _id } = req.user;
+          const { data } = await service.GetShopingDetails(_id);
+    
+          return res.json(data);
+        } catch (err) {
+          next(err);
+        }
+      });
+    
+      app.get("/wishlist", UserAuth, async (req, res, next) => {
+        try {
+          const { _id } = req.user;
+          const { data } = await service.GetWishList(_id);
+          return res.status(200).json({ results: data });
+        } catch (err) {
+          next(err);
+        }
+      });
+    
+};
